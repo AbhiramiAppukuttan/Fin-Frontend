4c159fceab1753a14e85f2561eee511f24405819
AbhiramiAppukuttan <abhiramiappukuttan09@gmail.com>
2025-04-28 13:34:12 +0530

    debug 19

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@
 // function App() {
 //   const userRole = useSelector((state)=>state.auth.role)
 //   return (
-//     <Router>
+//      <BrowserRouter>
 //       <Routes>
         
 //         {/* Public Pages */}
@@ -45,9 +45,9 @@
 //         <Route path="/signup" element={<><Navbar /><SignupPage /><Footer /></>} />
 //         <Route path="/forgot-password" element={<><Navbar /><ForgotPassword/><Footer /></>} />
 //         <Route path="/reset-password" element={<><Navbar /><ResetPassword/><Footer /></>} />
-
+// </Routes>
 //         {userRole==="individual"?(
-
+// <Routes>
 //         <Route path="/user">
 //         {/* User Dashboard Pages */}
 //               <Route path="dashboard" element={<><DashboardNavbar /><Dashboard /><Footer /></>} />
@@ -64,22 +64,21 @@
 //               <Route path="payment" element={<><DashboardNavbar /><PaymentPage/><Footer /></>} />
 
 //         </Route>
-//         ): (userRole==="admin")&&(
+//         </Routes>
+//         ): userRole==="admin"?
 
 //         {/* Admin Dashboard */}
-        
+//         <Routes>
 //         <Route path="/admin" element={<><AdminDashboard /><Footer /></>} />
 //         <Route path="/user-management" element={<><UserManagement /><Footer /></>} />
 //         <Route path="/feedback-support" element={<><FeedbackSupport /><Footer /></>} />
 //         <Route path="/admin/subscribers" element={<><Subscribers /><Footer /></>} />
 //         <Route path="/report" element={<><AdminReport /><Footer /></>} />
-
-//         )}
+// </Routes> :(
 
 //         <Route path="/*" element={<><DashboardNavbar /><PageNotFound/><Footer /></>} />
-
-//       </Routes>
-//     </Router>
+//       )}
+//     </BrowserRouter>
 //   );
 // }
 
@@ -88,7 +87,6 @@
 
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useSelector } from "react-redux";  // Make sure to import useSelector
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import DashboardNavbar from "./components/DashboardNavbar";
@@ -98,6 +96,9 @@ import About from "./pages/user/About";
 import Feature from "./pages/user/Feature";
 import Loginpage from "./pages/user/Loginpage";
 import SignupPage from "./pages/user/Signup";
+import ForgotPassword from "./pages/user/ForgotPassword";
+import ResetPassword from "./pages/user/ResetPassword";
+import PageNotFound from "./pages/user/PageNotFound";
 
 import Dashboard from "./pages/user/Dashboard";
 import Transaction from "./pages/user/Transaction";
@@ -105,42 +106,63 @@ import Budget from "./pages/user/Budget";
 import Saving from "./pages/user/Saving";
 import Report from "./pages/user/Report";
 import Profile from "./pages/user/Profile";
-
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import UserManagement from "./pages/admin/UserManagement";
-import FeedbackSupport from "./pages/admin/FeedbackSupport";
 import NotificationPage from "./pages/user/NotificationPage";
-import PageNotFound from "./pages/user/PageNotFound";
 import RecommendationsTips from "./pages/user/RecommendationsTips";
 import FeedbackForm from "./pages/user/FeedbackForm";
 import SubscriptionPage from "./pages/user/SubscriptionPage";
 import PaymentPage from "./pages/user/Paymentpage";
 import RecommendationSavings from "./pages/user/RecommendationSavings";
+
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import UserManagement from "./pages/admin/UserManagement";
+import FeedbackSupport from "./pages/admin/FeedbackSupport";
 import Subscribers from "./pages/admin/Subscribers";
 import AdminReport from "./pages/admin/AdminReport";
-import ForgotPassword from "./pages/user/ForgotPassword";
-import ResetPassword from "./pages/user/ResetPassword";
+
+import { useSelector } from "react-redux";
 
 function App() {
-  const userRole = useSelector((state) => state.auth?.payload?.role);  // Accessing user role from Redux state
-console.log(userRole);
+  const userRole = useSelector((state) => state.auth.role);
+
+  // Common Layouts
+  const PublicLayout = ({ children }) => (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+
+  const UserLayout = ({ children }) => (
+    <>
+      <DashboardNavbar />
+      {children}
+      <Footer />
+    </>
+  );
+
+  const AdminLayout = ({ children }) => (
+    <>
+      {children}
+      <Footer />
+    </>
+  );
 
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         {/* Public Pages */}
-        <Route path="/" element={<><Navbar /><Home /><Footer /></>} />
-        <Route path="/about" element={<><Navbar /><About /><Footer /></>} />
-        <Route path="/features" element={<><Navbar /><Feature /><Footer /></>} />
-        <Route path="/login" element={<><Navbar /><Loginpage /><Footer /></>} />
-        <Route path="/signup" element={<><Navbar /><SignupPage /><Footer /></>} />
-        <Route path="/forgot-password" element={<><Navbar /><ForgotPassword /><Footer /></>} />
-        <Route path="/reset-password" element={<><Navbar /><ResetPassword /><Footer /></>} />
+        <Route path="/" element={<PublicLayout><Home /></PublicLayout>} />
+        <Route path="/about" element={<PublicLayout><About /></PublicLayout>} />
+        <Route path="/features" element={<PublicLayout><Feature /></PublicLayout>} />
+        <Route path="/login" element={<PublicLayout><Loginpage /></PublicLayout>} />
+        <Route path="/signup" element={<PublicLayout><SignupPage /></PublicLayout>} />
+        <Route path="/forgot-password" element={<PublicLayout><ForgotPassword /></PublicLayout>} />
+        <Route path="/reset-password" element={<PublicLayout><ResetPassword /></PublicLayout>} />
 
-        {/* Conditional Routes for User Role */}
-        {userRole === "individual" ? (
-          <Route path="/" element={<><DashboardNavbar /><Footer /></>}>
-            {/* User Dashboard Pages */}
+        {/* User Pages */}
+        {userRole === "individual" && (
+          <Route path="/user" element={<UserLayout />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="transactions" element={<Transaction />} />
             <Route path="budget" element={<Budget />} />
@@ -154,21 +176,23 @@ console.log(userRole);
             <Route path="subscription" element={<SubscriptionPage />} />
             <Route path="payment" element={<PaymentPage />} />
           </Route>
-        ) : userRole === "admin" ? (
-          <Route path="/" element={<><Footer /></>}>
-            {/* Admin Dashboard */}
-            <Route path="admin" element={<><AdminDashboard /></>}/>
+        )}
+
+        {/* Admin Pages */}
+        {userRole === "admin" && (
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route path="dashboard" element={<AdminDashboard />} />
             <Route path="user-management" element={<UserManagement />} />
             <Route path="feedback-support" element={<FeedbackSupport />} />
             <Route path="subscribers" element={<Subscribers />} />
             <Route path="report" element={<AdminReport />} />
           </Route>
-        ) : null}
+        )}
 
-        {/* Catch All Route for Invalid Pages */}
-        <Route path="/*" element={<><PageNotFound /><Footer /></>} />
+        {/* 404 Page */}
+        <Route path="/*" element={<PublicLayout><PageNotFound /></PublicLayout>} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
